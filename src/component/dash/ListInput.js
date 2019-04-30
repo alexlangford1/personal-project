fc1681dd1df9b2877dda49fc7de1c81d4cd1f3d3
alex langford <alejandro010795@gmail.com>
2019-04-30 11:33:51 -0600

    c

--- src/component/dash/ListInput.js
+++ src/component/dash/ListInput.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import "./dash.css"
-import ActualEditInput from "./ActualEditInput"
+import ActualEditInput from "./actualEditInput"
 
 
 export default class ListInput extends Component {
